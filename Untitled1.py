f9d3f7205c440afbc2427607ffc387d7ff289281
Mallika-Regulla <153931718+Mallika-Regulla@users.noreply.github.com>
2024-07-11 15:36:05 +0530

    U

---
+++ Untitled1.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8b256136-3764-4dc0-83a1-50f0a3b099ae",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
